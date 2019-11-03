01b80c2eeaa1f4f7f48c430547df32b1434da4b7
Tomasz Kowalewski <tomassok@gmail.com>
2019-11-03 23:34:08 +0100

    Added custom color for chart, footer looks a bit better, added gradient background (looks ok, but a bit distracting), changed arrows to switch currencies to white color and made them bigger

--- src/app/model/chartconfig.model.ts
+++ src/app/model/chartconfig.model.ts
@@ -1,25 +1,39 @@
 import {ChartDataSets, ChartOptions} from 'chart.js';
-import {Label} from 'ng2-charts';
+import {Colors, Label} from 'ng2-charts';
 
 export class ChartConfig {
-  lineChartData: ChartDataSets[] = [
-    {data: [10, 20, 40, 60, 120, 30, 60], label: 'Wartości', yAxisID: 'y-axis-0'},
-  ];
+  lineChartData: ChartDataSets[] = [];
 
-  lineChartLabels: Label[] = ['dzien1', 'dzien2', 'dzien3', 'dzien4', 'dzien5', 'dzien6', 'dzien7'];
+  lineChartLabels: Label[] = [];
   lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
-      xAxes: [{}],
+      xAxes: [{
+        ticks: {fontColor: 'white'},
+        gridLines: {color: 'rgba(255,255,255,0.1)'}
+      }],
       yAxes: [
         {
           id: 'y-axis-0',
           position: 'left',
+          ticks: {fontColor: 'white'},
+          gridLines: {color: 'rgba(255,255,255,0.1)'}
         },
       ]
     },
     annotation: {},
   };
+
+  lineChartColor: Colors[] = [
+    {
+      backgroundColor: 'rgba(77,83,96,0.6)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,0.8)'
+    },
+  ];
 }
 
